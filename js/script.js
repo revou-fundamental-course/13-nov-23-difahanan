a4fa9a456bda938033c44711876fca2570630e65
difahanan <124042346+difahanan@users.noreply.github.com>
2023-12-18 06:25:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,3 +82,37 @@ function bannerSlide2() {
     document.getElementById("images-slider").src = image[b]
 }
 setInterval(bannerSlide2, 1000)
+
+// ========================================================================= try later 
+
+// const submitButton = document.getElementsByClassName('primary-button')
+// const nameInput = document.getElementById('name-input')
+// const emailInput = document.getElementById('email-input')
+// const nameOutput = document.getElementById('username')
+// const imageSlider = document.getElementById('slider')
+
+// submitButton[0].addEventListener("click", function(event) {
+//     if ( nameInput.value.length === 0 || emailInput.value.length === 0) {
+//         alert("Nama atau email tidak boleh kosong")
+//     } else {
+//         nameOutput.textContent = nameInput.value
+//     }
+// })
+
+// var i = 0
+// const images = [
+//     "assets/images-1.jpeg",
+//     "assets/images-2.jpeg",
+//     "assets/images-3.jpg"
+// ]
+
+// function bannerSlide() {
+//     i += 1
+//     if (i == images.length) {
+//         i = 0
+//     }
+//     imagesSlider.src = images[i]
+// }
+// setInterval(bannerSlide, 1000)
+// =========================================================================
+
